9ddccf1f182f60630d87d7cad92ff1bdec646c09
Sergey Kolesnik <sergey.kolesnik.ld@gmail.com>
2020-12-04 13:23:49 +0500

    refactoring: - new header include/pod_reflection/pod_reflection.h - added test.cpp - implemented is_aggregate_initiazable

---
+++ include/pod_reflection/pod_reflection.h
@@ -0,0 +1,79 @@
+﻿#pragma once
+
+#include <cstddef>
+#include <tuple>
+#include <type_traits>
+
+namespace eld
+{
+    namespace detail
+    {
+        template<typename... Ts> struct make_void
+        {
+            typedef void type;
+        };
+        template<typename... Ts> using void_t = typename make_void<Ts...>::type;
+
+        /*!
+         * \TODO: description
+         */
+        template<size_t>
+        struct convertible_to_any
+        {
+            // Any must be default constructable
+            template<typename Any>
+            constexpr operator Any() const noexcept;
+//            {
+//                return Any();
+//            }
+        };
+
+        /*!
+         * \TODO: description
+         * @tparam Allowed
+         */
+        template<typename Allowed>
+        struct convertible_to_one
+        {
+            template<typename Any>
+            constexpr operator Any() = delete;
+
+            constexpr operator Allowed() const noexcept;
+        };
+
+        /*!
+         * \TODO: description
+         * @tparam POD
+         * @tparam Tuple
+         */
+        template<typename POD, typename Tuple, typename = void_t<>>
+        struct is_aggregate_initializable_from_tuple : std::false_type
+        {
+        };
+
+        /*!
+         * \TODO: description
+         * @tparam POD
+         * @tparam Types
+         */
+        template<typename POD, typename ... Types>
+        struct is_aggregate_initializable_from_tuple<POD,
+                std::tuple<Types...>,
+                void_t<decltype(POD{std::declval<Types>()...})>>
+                : std::true_type
+        {
+        };
+
+        /*!
+         * \TODO: description
+         * @tparam T
+         * @tparam From
+         */
+        template<typename T, typename ... From>
+        struct is_aggregate_initializable : is_aggregate_initializable_from_tuple<T, std::tuple<From...>>
+        {
+        };
+
+    }
+
+}
---
+++ tests/test.cpp
@@ -0,0 +1,49 @@
+﻿#include <pod_reflection/pod_reflection.h>
+
+#include <string>
+
+struct abc
+{
+    int a;// = 4;
+    float b;// = 8.15f;        //vs 17 express does not allow implicit conversion from double
+    char c,// = 'c',
+            d;// = 'd';
+    int e;// = 16;
+    double f;// = 23.42;
+    std::string g;// = "oceanic";
+};
+
+struct dce
+{
+    int a;// = 4;
+    double b;// = 3.18;
+    abc c;//{};
+};
+
+using namespace eld;
+
+#include <iostream>
+
+int main()
+{
+    decltype(abc{}) abc1;
+    decltype(abc{detail::convertible_to_any<0>()}) abc2;
+
+//    static_assert(detail::is_aggregate_initializable_from_tuple<abc, std::tuple<int>>(), "Unexpected");
+
+    constexpr bool b = detail::is_aggregate_initializable_from_tuple<abc, std::tuple<int>>();
+    std::cout << b << std::endl;
+    static_assert(detail::is_aggregate_initializable<abc>(), "Unexpected");
+    static_assert(detail::is_aggregate_initializable<abc, int>(), "Unexpected");
+    static_assert(detail::is_aggregate_initializable<abc, int, float>(), "Unexpected");
+    static_assert(detail::is_aggregate_initializable<abc, int, float, char>(), "Unexpected");
+    static_assert(detail::is_aggregate_initializable<abc, int, float, char, char>(), "Unexpected");
+    static_assert(detail::is_aggregate_initializable<abc, int, float, char, char, int>(), "Unexpected");
+    static_assert(detail::is_aggregate_initializable<abc, int, float, char, char, int, double>(), "Unexpected");
+    static_assert(detail::is_aggregate_initializable<abc, int, float, char, char, int, double, std::string>(), "Unexpected");
+    static_assert(detail::is_aggregate_initializable<abc, abc>(), "Unexpected");
+
+    // constexpr size_t argcAbc = detail::args_counter<abc>::value();
+
+    return 0;
+}
