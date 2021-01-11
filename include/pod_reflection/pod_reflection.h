68b6e770ed6d85c881edd86df27c9b4ce93dba8c
Sergey Kolesnik <sergey.kolesnik.ld@gmail.com>
2021-01-11 13:46:15 +0500

    defined public classes. Implemented pod_size and pod_element

--- include/pod_reflection/pod_reflection.h
+++ include/pod_reflection/pod_reflection.h
@@ -9,6 +9,12 @@ namespace eld
 {
     namespace detail
     {
+
+        template<size_t>
+        struct tag_s
+        {
+        };
+
         template<typename... Ts> struct make_void
         {
             typedef void type;
@@ -18,10 +24,8 @@ namespace eld
         /*!
          * \TODO: description
          */
-        // template<size_t>
         struct implicitly_convertible
         {
-            // Any must be default constructable?
             template<typename Any>
             constexpr operator Any() const noexcept;
         };
@@ -41,7 +45,6 @@ namespace eld
 
             constexpr operator Allowed() const noexcept;
         };
-
 
         // index sequence only
         template<size_t ...>
@@ -63,62 +66,43 @@ namespace eld
         template<size_t N>
         using make_index_sequence = typename index_sequence_helper<N>::type;
 
-        /*!
-         * \TODO: description
-         * @tparam POD
-         * @tparam Tuple
-         */
-        template<typename POD, typename Tuple, typename = void_t<>>
-        struct is_aggregate_initializable_from_tuple : std::false_type
+        template<typename, typename T, typename ...>
+        struct is_aggregate_initialisable_ : std::false_type
         {
         };
 
-        /*!
-         * \TODO: description
-         * @tparam POD
-         * @tparam Types
-         */
-        template<typename POD, typename ... Types>
-        struct is_aggregate_initializable_from_tuple<POD,
-                std::tuple<Types...>,
-                void_t<decltype(POD{std::declval<Types>()...})>>
-                : std::true_type
-        {
-        };
-
-        template<typename Tuple, typename Add>
-        using tuple_push_back = decltype(std::tuple_cat(Tuple(), std::tuple<Add>()));
-
-        /*!
-         * \TODO: description
-         * @tparam T
-         * @tparam From
-         */
         template<typename T, typename ... From>
-        struct is_aggregate_initializable : is_aggregate_initializable_from_tuple<T, std::tuple<From...>>
+        struct is_aggregate_initialisable_<
+                void_t<decltype(T{std::declval<From>()...})>,
+                T,
+                From...> : std::true_type
         {
-            constexpr static size_t args_count = sizeof...(From);
         };
 
-        template<typename POD, size_t N, typename = make_index_sequence<N>>
-        struct is_aggregate_initializable_from_n_args;
+        template<typename T, typename ... From>
+        using is_aggregate_initialisable = is_aggregate_initialisable_<void_t<>, T, From...>;
 
+        // TODO: remove?
+        template<typename POD, size_t N, typename = make_index_sequence<N>>
+        struct is_aggregate_initialisable_from_n_args;
+
+        // TODO: remove?
         template<typename POD, size_t N, size_t ... Indx>
-        struct is_aggregate_initializable_from_n_args<POD, N, index_sequence<Indx...>> :
-                is_aggregate_initializable<POD, implicitly_convertible_s<Indx>...>
+        struct is_aggregate_initialisable_from_n_args<POD, N, index_sequence<Indx...>> :
+                is_aggregate_initialisable<POD, implicitly_convertible_s<Indx>...>
         {
         };
 
         template<typename POD, typename T, size_t PODMemberIndex,
                 typename = void,
                 typename = make_index_sequence<PODMemberIndex>>
-        struct is_pod_member_initializable_from_t : std::false_type
+        struct is_pod_member_initialisable_from_t : std::false_type
         {
         };
 
         template<typename POD, typename T, size_t PODMemberIndex,
                 size_t ... PrevArgs>
-        struct is_pod_member_initializable_from_t<POD,
+        struct is_pod_member_initialisable_from_t<POD,
                 T,
                 PODMemberIndex,
                 void_t<decltype(POD{implicitly_convertible_s<PrevArgs>()..., explicitly_convertible<T>()})>,
@@ -126,92 +110,135 @@ namespace eld
         {
         };
 
+        /*!
+     * Helper class to count maximum arguments for aggregate initialization of a POD type
+     * @tparam POD
+     */
+        template<typename POD>
+        class aggregate_args_counter
+        {
+        public:
+            constexpr static size_t value()
+            {
+                using namespace detail;
+                return sum_increment(tag_s<0>(), std::integral_constant<bool,
+                        (bool) is_aggregate_initialisable_from_n_args<POD, 0>()>());
+            }
+
+        private:
+
+            template<size_t i>
+            constexpr static size_t sum_increment(tag_s<i>, std::false_type)
+            {
+                return i - 1;
+            }
+
+            template<size_t i>
+            constexpr static size_t sum_increment(tag_s<i>, std::true_type)
+            {
+                using namespace detail;
+                return sum_increment(tag_s<i + 1>(),
+                                     std::integral_constant<bool,
+                                             (bool) is_aggregate_initialisable_from_n_args<POD, i + 1>()>());
+            }
+        };
+
+        template<size_t I, typename POD, typename TupleFeed>
+        class tuple_index_from_pod_member
+        {
+            static_assert(std::tuple_size<TupleFeed>(), "TupleFeed must not be empty!");
+
+        public:
+
+            // TODO: remove
+            template<typename T>
+            using is_initializable = std::integral_constant<bool,
+                    (bool) detail::is_pod_member_initialisable_from_t<POD, T, I>()>;
+
+            constexpr static size_t value()
+            {
+                return find_type(detail::tag_s<0>(),
+                                 is_initializable<typename std::tuple_element<0, TupleFeed>::type>());
+            }
+
+        private:
+
+            // stop case on failure when tuple index exceeded
+            constexpr static size_t find_type(detail::tag_s<std::tuple_size<TupleFeed>::value - 1>,
+                                              std::false_type)
+            {
+                return size_t() - 1;
+            }
+
+            // stop case on success
+            template<size_t tupleIndx>
+            constexpr static size_t find_type(detail::tag_s<tupleIndx>, std::true_type)
+            {
+                return tupleIndx;
+            }
+
+            // general recursion
+            template<size_t TupleIndex>
+            constexpr static size_t find_type(detail::tag_s<TupleIndex>, std::false_type)
+            {
+                return find_type(detail::tag_s<TupleIndex + 1>(),
+                                 is_initializable<typename std::tuple_element<TupleIndex + 1, TupleFeed>::type>());
+            }
+        };
+
     }
 
+
+    // PUBLIC classes
     /*!
-     * \TODO: description
+     * Provides access to the number of elements in a POD type as a compile-time constant expression
      * @tparam POD
      */
     template<typename POD>
-    class aggregate_args_counter
+    struct pod_size : public std::integral_constant<size_t, detail::aggregate_args_counter<POD>::value()>
     {
-    public:
-        constexpr static size_t value()
-        {
-            using namespace detail;
-            return sum_increment(tag_s<0>(), std::integral_constant<bool,
-                    (bool) is_aggregate_initializable_from_n_args<POD, 0>()>());
-        }
-
-        constexpr operator size_t() const
-        {
-            return value();
-        }
-
-    private:
-        template<size_t> struct tag_s
-        {
-        };
-
-        template<size_t i>
-        constexpr static size_t sum_increment(tag_s<i>, std::false_type)
-        {
-            return i - 1;
-        }
-
-        template<size_t i>
-        constexpr static size_t sum_increment(tag_s<i>, std::true_type)
-        {
-            using namespace detail;
-            return sum_increment(tag_s<i + 1>(),
-                                 std::integral_constant<bool,
-                                         (bool) is_aggregate_initializable_from_n_args<POD, i + 1>()>());
-        }
     };
 
-//    template<typename POD, size_t MemberIndex,
-//            typename Tuple,
-//            typename = detail::make_index_sequence<MemberIndex>>
-//    class get_pod_member_type;
-//
-//    template<typename POD,
-//            typename Tuple,
-//            // typename ... FeedArgs,
-//            size_t MemberIndex,
-//            size_t ... Indx>
-//    class get_pod_member_type<POD,
-//            MemberIndex,
-//            Tuple,
-//            // std::tuple<FeedArgs...>,
-//            detail::index_sequence<Indx...>>
-//    {
-//    public:
-//
-//    private:
-//
-//        template <size_t> struct tag_s{};
-//
-//        // out-of-tuple-range stop case method
-//        constexpr static size_t find_type(tag_s<std::tuple_size<Tuple>::value>,
-//                std::false_type)
-//        {
-//            return size_t() - 1;
-//        }
-//
-//        // stop case method
-//        template <size_t IndexFound>
-//        constexpr static size_t find_type(tag_s<IndexFound>, std::true_type)
-//        {
-//            return IndexFound;
-//        }
-//
-//        // recursive method
-//        template <size_t I>
-//        constexpr static size_t find_type(tag_s<I>, std::false_type)
-//        {
-//            using TupleTypeI = typename std::tuple_element<I, Tuple>::type;
-//            return find_type(tag_s<I + 1>(),
-//                    std::integral_constant<bool, std::is_same<>>)
-//        }
-//    };
+    /*!
+     * Tag for a type that couldn't be deduced from provided tuple feed
+     */
+    struct undeduced;
+
+    namespace detail
+    {
+        template<size_t I, typename TupleFeed, bool = I >= std::tuple_size<TupleFeed>::value>
+        struct deduced_tuple_elem
+        {
+            using type = undeduced;
+        };
+
+        template<size_t I, typename TupleFeed>
+        struct deduced_tuple_elem<I, TupleFeed, false>
+        {
+            using type = typename std::tuple_element<I, TupleFeed>::type;
+        };
+    }
+
+    /*!
+     * Provides compile-time indexed access to the types of the elements of the POD type deduced from
+     * the provided TupleFeed class
+     * @tparam I - index of the element
+     * @tparam POD - POD type
+     * @tparam TupleFeed - Tuple of types to be used as a feed to deduce an element
+     */
+    template<size_t I, typename POD, typename TupleFeed>
+    struct pod_element
+    {
+        static_assert(std::tuple_size<TupleFeed>(), "TupleFeed must not be empty!");
+
+        using type = typename
+        detail::deduced_tuple_elem<detail::tuple_index_from_pod_member<I, POD, TupleFeed>::value(),
+                TupleFeed>::type;
+    };
+
+    template<size_t I, typename POD, typename TupleFeed>
+    using pod_element_t = typename pod_element<I, POD, TupleFeed>::type;
+
+    /// TODO: get<indx, tuple_feed>(pod)
+
 }
--- tests/test.cpp
+++ tests/test.cpp
@@ -75,7 +75,7 @@ int main()
 
     detail::make_index_sequence<5> sdgb;
 
-    constexpr bool f = detail::is_aggregate_initializable_from_n_args<abc, 6>();
+    constexpr bool f = detail::is_aggregate_initialisable_from_n_args<abc, 6>();
 
     constexpr size_t zero = accumulate_decrement(tag_s<0>(), 16);
     constexpr size_t sum = accumulate_decrement(tag_s<5>(), 5);
@@ -88,40 +88,79 @@ int main()
     decltype(abc{}) abc1;
     decltype(abc{detail::implicitly_convertible()}) abc2;
 
-    static_assert(std::is_same<std::tuple<int, double>, detail::tuple_push_back<std::tuple<int>, double>>::value,
-                  "Failed to push_back to tuple");
-
-//    static_assert(detail::is_aggregate_initializable_from_tuple<abc, std::tuple<int>>(), "Unexpected");
-
-    constexpr bool b = detail::is_aggregate_initializable_from_tuple<abc, std::tuple<int>>();
-    std::cout << b << std::endl;
-    static_assert(detail::is_aggregate_initializable<abc>(), "Unexpected");
-    static_assert(detail::is_aggregate_initializable<abc, int>(), "Unexpected");
-    static_assert(detail::is_aggregate_initializable<abc, int, float>(), "Unexpected");
-    static_assert(detail::is_aggregate_initializable<abc, int, float, char>(), "Unexpected");
-    static_assert(detail::is_aggregate_initializable<abc, int, float, char, char>(), "Unexpected");
-    static_assert(detail::is_aggregate_initializable<abc, int, float, char, char, int>(), "Unexpected");
-    static_assert(detail::is_aggregate_initializable<abc, int, float, char, char, int, double>(), "Unexpected");
-    static_assert(detail::is_aggregate_initializable<abc, int, float, char, char, int, double, std::string>(),
+//    static_assert(std::is_same<std::tuple<int, double>, detail::tuple_push_back<std::tuple<int>, double>>::value,
+//                  "Failed to push_back to tuple");
+//
+////    static_assert(detail::is_aggregate_initialisable_from_tuple<abc, std::tuple<int>>(), "Unexpected");
+//
+//    constexpr bool b = detail::is_aggregate_initialisable_from_tuple<abc, std::tuple<int>>();
+//    std::cout << b << std::endl;
+    static_assert(detail::is_aggregate_initialisable<abc>(), "Unexpected");
+    static_assert(detail::is_aggregate_initialisable<abc, int>(), "Unexpected");
+    static_assert(detail::is_aggregate_initialisable<abc, int, float>(), "Unexpected");
+    static_assert(detail::is_aggregate_initialisable<abc, int, float, char>(), "Unexpected");
+    static_assert(detail::is_aggregate_initialisable<abc, int, float, char, char>(), "Unexpected");
+    static_assert(detail::is_aggregate_initialisable<abc, int, float, char, char, int>(), "Unexpected");
+    static_assert(detail::is_aggregate_initialisable<abc, int, float, char, char, int, double>(), "Unexpected");
+    static_assert(detail::is_aggregate_initialisable<abc, int, float, char, char, int, double, std::string>(),
                   "Unexpected");
-    static_assert(detail::is_aggregate_initializable<abc, abc>(), "Unexpected");
+    static_assert(detail::is_aggregate_initialisable<abc, abc>(), "Unexpected");
 
-    static_assert(detail::is_aggregate_initializable<abc, detail::implicitly_convertible, detail::implicitly_convertible>(),
-                  "Unexpected");
+    static_assert(
+            detail::is_aggregate_initialisable<abc, detail::implicitly_convertible, detail::implicitly_convertible>(),
+            "Unexpected");
 
-    static_assert(eld::aggregate_args_counter<abc>() == 7, "Invalid arguments number!");
-    static_assert(eld::aggregate_args_counter<none>() ==
+    static_assert(eld::pod_size<abc>() == 7, "Invalid arguments number!");
+    static_assert(eld::pod_size<none>() ==
                   std::numeric_limits<size_t>::max(), "Invalid arguments number!");
 
-    static_assert(eld::detail::is_pod_member_initializable_from_t<abc, int, 0>(),
-            "Unexpected result");
-    static_assert(eld::detail::is_pod_member_initializable_from_t<abc, float, 1>(),
+    static_assert(eld::detail::is_pod_member_initialisable_from_t<abc, int, 0>(),
                   "Unexpected result");
-    static_assert(eld::detail::is_pod_member_initializable_from_t<abc, std::string, 6>(),
+    static_assert(eld::detail::is_pod_member_initialisable_from_t<abc, float, 1>(),
+                  "Unexpected result");
+    static_assert(eld::detail::is_pod_member_initialisable_from_t<abc, std::string, 6>(),
                   "Unexpected result");
 
-    static_assert(!eld::detail::is_pod_member_initializable_from_t<abc, void, 0>(),
+    static_assert(!eld::detail::is_pod_member_initialisable_from_t<abc, void, 0>(),
                   "Unexpected result");
+
+    using TupleFeedAbc = std::tuple<int, float, double, char, std::string>;
+
+//    static_assert(eld::detail::tuple_index_from_pod_member<0, abc, std::tuple<int>>::value() == 0,
+//                  "");
+//    static_assert(eld::detail::tuple_index_from_pod_member<0, abc, std::tuple<double, int>>::value() == 1,
+//                  "");
+//    static_assert(eld::detail::tuple_index_from_pod_member<0, abc, std::tuple<double>>::value() == (size_t) -1,
+//                  "");
+//
+//    static_assert(eld::detail::tuple_index_from_pod_member<1, abc, std::tuple<float>>::value() == 0,
+//                  "");
+//    static_assert(eld::detail::tuple_index_from_pod_member<1, abc, std::tuple<int, float>>::value() == 1,
+//                  "");
+//    static_assert(eld::detail::tuple_index_from_pod_member<1, abc, std::tuple<double>>::value() == (size_t) -1,
+//                  "");
+
+    // successful search
+
+    constexpr size_t int_indx_0 =
+            eld::detail::tuple_index_from_pod_member<0, abc, std::tuple<int>>::value();
+    static_assert(int_indx_0 == 0, "");
+    static_assert(std::is_same<int, eld::pod_element_t<0, abc, std::tuple<int>>>::value,
+            "");
+
+    constexpr size_t int_indx_1 =
+            eld::detail::tuple_index_from_pod_member<0, abc, std::tuple<double, int>>::value();
+    static_assert(int_indx_1 == 1, "");
+    static_assert(std::is_same<int, eld::pod_element_t<0, abc, std::tuple<double, int>>>::value,
+                  "");
+
+    constexpr size_t float_indx =
+        eld::detail::tuple_index_from_pod_member<1, abc, std::tuple<int, float>>::value();
+//    constexpr size_t float_indx_notfound =
+//        eld::detail::tuple_index_from_pod_member<1, abc, std::tuple<int>>::value();
+
+    static_assert(std::is_same<std::string,
+            eld::pod_element_t<6, abc, TupleFeedAbc>>::value, "Failed to deduce std::string in abc");
 
     return 0;
 }
